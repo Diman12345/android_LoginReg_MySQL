24a04e4660073fcd7893ca429682b7d1689fcd15
Diman12345 <wardimanperdian@gmail.com>
2018-11-08 16:31:18 +0700

    new
    package

---
+++ loginMySQL/app/src/main/java/wardiman/com/loginmysql/Server.java
@@ -0,0 +1,8 @@
+package wardiman.com.loginmysql;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Server {
+    public static final String URL = "http://192.168.31.34/login/";
+}
